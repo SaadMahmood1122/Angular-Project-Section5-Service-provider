33134e2a78ad27b38dbcc659b07c57f3aa32e862
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-14 02:57:55 +0500

    added ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
   currentStatus = 'online';
+  private intervel?: ReturnType<typeof setInterval>;
   ngOnInit() {
     console.log('ngOnInit()......');
-    setInterval(() => {
+    this.intervel = setInterval(() => {
       this.currentStatus =
         this.currentStatus === 'online' ? 'offline' : 'online';
     }, 2000); // Changes status every second
+  }
+
+  ngAfterViewInit() {
+    console.log('View Init');
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.intervel);
   }
 }
